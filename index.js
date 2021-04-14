c3e4f6ff5d20aa394ead8ec116d82451eee78032
cassyoutofwater <57638980+cassyoutofwater@users.noreply.github.com>
2021-04-14 06:12:37 -0700

    Task 4 completed

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({name: "Andy", rating: 5, feedback:"Imaginary Review"})
+console.log(reviews)
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
